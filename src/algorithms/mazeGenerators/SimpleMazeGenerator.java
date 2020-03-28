1f81910e1461311fcc0f98a2346023dcc3a17521
morzw <morzw@post.bgu.ac.il>
2020-03-28 17:34:30 +0300

    SimpleMazeGenerator

--- src/algorithms/mazeGenerators/SimpleMazeGenerator.java
+++ src/algorithms/mazeGenerators/SimpleMazeGenerator.java
@@ -1,14 +1,23 @@
 package algorithms.mazeGenerators;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class SimpleMazeGenerator extends AMazeGenerator
 {
     @Override
     public Maze generate(int rows, int cols) {
         Maze simpleMaze = new Maze(rows, cols);
+        int rand;
         for (int i=0; i<rows; i++)
         {
             for (int j=0; j<cols; j++)
-                simpleMaze.getMaze()[i][j] = '0';
+            {
+                rand = ThreadLocalRandom.current().nextInt(0, 11);
+                if (rand < 10 )
+                    simpleMaze.getMaze()[i][j] = '0';
+                else
+                    simpleMaze.getMaze()[i][j] = '1';
+            }
         }
         chooseStartandGoal(simpleMaze, rows, cols);
         return simpleMaze;
