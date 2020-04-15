11ed7443089c6b8e683eaa69d0ae0abd90d70970
morzw <morzw@post.bgu.ac.il>
2020-04-15 12:29:24 +0300

    changed SimpleMazeGenerator smaller than 3 (or equal) to empty maze

--- src/algorithms/mazeGenerators/SimpleMazeGenerator.java
+++ src/algorithms/mazeGenerators/SimpleMazeGenerator.java
@@ -13,16 +13,25 @@ public class SimpleMazeGenerator extends AMazeGenerator
         if (cols<2)
             cols=2;
         Maze simpleMaze = new Maze(rows, cols);
-        int rand;
-        for (int i=0; i<rows; i++)
-        {
-            for (int j=0; j<cols; j++)
-            {
-                rand = ThreadLocalRandom.current().nextInt(0, 11);
-                if (rand < 10 )
+        if (rows <= 3 || cols <= 3) {
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++)
                     simpleMaze.getMaze()[i][j] = 0;
-                else
-                    simpleMaze.getMaze()[i][j] = 1;
+            }
+        }
+        else
+        {
+            int rand;
+            for (int i=0; i<rows; i++)
+            {
+                for (int j=0; j<cols; j++)
+                {
+                    rand = ThreadLocalRandom.current().nextInt(0, 11);
+                    if (rand < 10 )
+                        simpleMaze.getMaze()[i][j] = 0;
+                    else
+                        simpleMaze.getMaze()[i][j] = 1;
+                }
             }
         }
         chooseStartandGoal(simpleMaze, rows, cols);
